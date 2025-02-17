13abaf1407b33112959e1912a94e78215275061b
SeYn0604 <lkjn111@naver.com>
2025-02-18 01:43:30 +0900

    1
    1

--- Assets/0.scripts/CharacterAnimation.cs
+++ Assets/0.scripts/CharacterAnimation.cs
@@ -11,12 +11,13 @@ public class Runner : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
-        // 시작 시 강제로 run 애니메이션 재생
-        animator.Play("run");
+        // 시작 시 run 애니메이션 재생
+        //animator.Play("run", 0);
     }
 
     void Update()
     {
+        /*      
         float move = Input.GetAxis("Horizontal");
 
         // 좌우 이동
@@ -31,5 +32,8 @@ public class Runner : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+        */ //캐릭터 수동이동, 생각해보니 단방향 자동이동으로 짜면 더 편할듯
+        // 무조건 오른쪽으로 자동 이동
+        rb.velocity = new Vector2(speed, rb.velocity.y);
     }
 }
